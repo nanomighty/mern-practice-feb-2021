160ed4e23f0d17f959b445da7359577ea53b6216
nanomighty <nancytranlewis@gmail.com>
2021-02-09 21:54:37 -0800

    Added Predict Destructuring Assignment

---
+++ PredictDestructuring.js
@@ -0,0 +1,65 @@
+//Assignment : Predict Destructuring
+
+
+//Problem1: 
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [randomCar] = cars
+const [, otherRandomCar] = cars
+//Predict the output
+console.log(randomCar) // logs 'Tesla'
+console.log(otherRandomCar) // logs 'Mercedes'
+
+
+//Problem2: 
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);       //Reference Error - name is not defined
+console.log(otherName);  //logs 'Elon'
+
+
+//Problem3: 
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;
+//Predict the output
+console.log(password);           //logs 12345
+console.log(hashedPassword);     //logs undefined since there are no key called password
+
+
+//Problem4: 
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [, first] = numbers;   // first = 2
+const [, , , second] = numbers; // second = 5
+const [, , , , , , , , third] = numbers;  // third = 2
+//Predict the output
+console.log(first == second);   //logs false
+console.log(first == third);    //logs true
+
+
+//Problem 5: 
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;            //key = 'value'
+const { secondKey } = lastTest;      //secondKey = [1, 5, 1, 8, 3, 3]
+const [, willThisWork] = secondKey;  //willThisWork = 5
+//Predict the output
+console.log(key);                    //logs 'value'
+console.log(secondKey);              //logs [1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]);           //logs 1
+console.log(willThisWork);           //logs 5
+
+
+
+
+
