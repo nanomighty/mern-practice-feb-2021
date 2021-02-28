57e8144a67d105d6d6b59cf7d220238610582542
nanomighty <nancytranlewis@gmail.com>
2021-02-28 13:29:26 -0800

    CodingDojo version of solving Box Generator Assgmt

--- boxgenerator/src/App.js
+++ boxgenerator/src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Form from './Components/Form'
+import Box from './Components/Box'
 
 function App() {
+  const [box, setBox] = setState({
+    colors: [],
+  });
+
   return (
     <div className="App">
-        <Form />
-
+      <div className="container">
+          <h1 className="display-4 my-3">Box Generator</h1>
+          <Form setBox = {setBox} box = {box} />
+          <div className='row'>
+            {box.colors.map((color) => (
+              <Box color={color} />
+            ))}
+          </div>
+      </div>
     </div>
   );
 }
--- boxgenerator/src/Components/Form.js
+++ boxgenerator/src/Components/Form.js
@@ -1,37 +1,35 @@
 import React, {useState} from 'react'
 
-const Form = (props) => {
+const Form = ({setBox, box}) => {
     const [color, setColor] = useState("");
-    const [box, setBox] = useState({
-        colors: [],
-    })
 
-    const handleAdd = (e) => {
-        e.preventDefault();
+    const onChange = (e) => {
+        (e) => setColor(e.target.value);
+    }
+
+    const handleAdd = () => {
         setBox({
             ...box,
             colors: [...box.colors , color] 
         });
         setColor("");
-    }
+    };
     
     return (
-        <div>
-            <label htmlFor="clr">Color</label>
-            <input type="text" name="clr" id="clr" value = { color } onChange = { 
-                (e) => setColor(e.target.value)}>
-                </input>
-            <button onClick={ handleAdd }>Add</button>
-            <div>
-                {
-                    box.colors.map((color) => {
-                    return <div className='generatedBox' style={{ backgroundColor: color}}> </div>
-                    })
-                }
+        <div class="input-group mb-3">
+            <input 
+                onChange = {onChange}
+                type="text"
+                class="form-control"
+                name="color"
+                value={color}
+            />
+            <div class="input-group-append">
+                <button onClick={handleAdd} class="btn btn-primary">
+                    Add
+                </button>
             </div>
         </div>
-
-
     );
 };
 export default Form;
