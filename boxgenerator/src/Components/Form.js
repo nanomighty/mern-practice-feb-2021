80f5f62180bc1dd1ef62d73fc22e481e24629ca6
nanomighty <nancytranlewis@gmail.com>
2021-02-28 12:00:34 -0800

    Added Box Generator Assignment

---
+++ boxgenerator/src/Components/Form.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+const Form = (props) => {
+    const [color, setColor] = useState("");
+    const [box, setBox] = useState({
+        colors: [],
+    })
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+        setBox({
+            ...box,
+            colors: [...box.colors , color] 
+        });
+        setColor("");
+    }
+    
+    return (
+        <div>
+            <label htmlFor="clr">Color</label>
+            <input type="text" name="clr" id="clr" value = { color } onChange = { 
+                (e) => setColor(e.target.value)}>
+                </input>
+            <button onClick={ handleAdd }>Add</button>
+            <div>
+                {
+                    box.colors.map((color) => (
+                    <div className='generatedBox' style={{ backgroundColor: color}}> </div>
+                    ))
+                }
+            </div>
+        </div>
+
+
+    );
+};
+export default Form;
--- moreforms/src/Components/UserForm.js
+++ moreforms/src/Components/UserForm.js
@@ -10,76 +10,76 @@ const UserForm = (props) => {
     }
 
 
-const checkFirstName = () =>{
-    if (inputs.firstName.length > 0 && inputs.firstName.length < 2){
+    const checkFirstName = () =>{
+        if (inputs.firstName.length > 0 && inputs.firstName.length < 2){
+            return(
+                "First Name must be at least 2 characters"
+            )
+        }
+    };
+
+    const checkLastName = () => {
+        if (inputs.lastName.length > 0 && inputs.lastName.length < 2){
+            return (
+                "Last Name must be at least 2 characters"
+            )
+        }
+    }
+
+    const checkEmail = () =>{
+        if (inputs.email.length > 0 && inputs.email.length < 2) {
+            return (
+                "Email must be at least 2 characters"
+            )
+        }
+    } 
+
+    const checkPassword = () => {
+        if (inputs.password.length > 0 && inputs.password.length < 8) {
+            return (
+                "Password must be at least 8 characters"
+            )
+        }
+    }
+
+    const matchPassword = () => {
+        if (inputs.password !== inputs.confirmPassword){
+            return (
+                "Passwords must match"
+            )
+        }
+    }
+
+
         return(
-            "First Name must be at least 2 characters"
+            <form onsubmit={(e) => e.preventDefault()}>
+                <div class="inputBox">
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" name="firstName" id="firstName" placeholder="Search..." onChange = {inputChange}/>
+                </div>
+                <h6>{checkFirstName()}</h6>
+                <div class="inputBox">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" name="lastName" id="lastName" placeholder="Search..." onChange={inputChange}/>
+                </div>
+                <h6>{checkLastName()}</h6>
+                <div class="inputBox">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Search..." onChange={inputChange}/>
+                </div>
+                <h6>{checkEmail()}</h6>
+                <div class="inputBox">
+                    <label htmlFor = "password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Search..." onChange={inputChange}/>
+                </div>
+                <h6>{checkPassword()}</h6>
+                <div class="inputBox">
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Search..." onChange={inputChange} />
+                </div>
+                <h6>{matchPassword()}</h6>
+            </form>
+
         )
-    }
-};
-
-const checkLastName = () => {
-    if (inputs.lastName.length > 0 && inputs.lastName.length < 2){
-        return (
-            "Last Name must be at least 2 characters"
-        )
-    }
-}
-
-const checkEmail = () =>{
-    if (inputs.email.length > 0 && inputs.email.length < 2) {
-        return (
-            "Email must be at least 2 characters"
-        )
-    }
-} 
-
-const checkPassword = () => {
-    if (inputs.password.length > 0 && inputs.password.length < 8) {
-        return (
-            "Password must be at least 8 characters"
-        )
-    }
-}
-
-const matchPassword = () => {
-    if (inputs.password !== inputs.confirmPassword){
-        return (
-            "Passwords must match"
-        )
-    }
-}
-
-
-    return(
-        <form onsubmit={(e) => e.preventDefault()}>
-            <div class="inputBox">
-                <label htmlFor="firstName">First Name</label>
-                <input type="text" name="firstName" id="firstName" placeholder="Search..." onChange = {inputChange}/>
-            </div>
-            <h6>{checkFirstName()}</h6>
-            <div class="inputBox">
-                <label htmlFor="lastName">Last Name</label>
-                <input type="text" name="lastName" id="lastName" placeholder="Search..." onChange={inputChange}/>
-            </div>
-            <h6>{checkLastName()}</h6>
-            <div class="inputBox">
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Search..." onChange={inputChange}/>
-            </div>
-            <h6>{checkEmail()}</h6>
-            <div class="inputBox">
-                <label htmlFor = "password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Search..." onChange={inputChange}/>
-            </div>
-            <h6>{checkPassword()}</h6>
-            <div class="inputBox">
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Search..." onChange={inputChange} />
-            </div>
-            <h6>{matchPassword()}</h6>
-        </form>
-
-    )
 }
 export default UserForm;
