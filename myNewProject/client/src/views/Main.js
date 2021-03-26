0d45eeb080c07158aa21aa55ff4b3d5b75083af2
nanomighty <nancytranlewis@gmail.com>
2021-03-25 17:12:01 -0700

    first run through of full stack mern project

---
+++ myNewProject/client/src/views/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersonForm from '../components/PersonForm';
+
+
+const Main = () => {
+    return (
+        <div>
+            <PersonForm/>
+        </div>
+    )
+}
+
+export default Main;
