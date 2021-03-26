9e06d70818e947916abbd70729b42b3bd7ce37db
nanomighty <nancytranlewis@gmail.com>
2021-03-25 20:05:05 -0700

    Added the Main component

--- productManagerProject/client/src/App.js
+++ productManagerProject/client/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
-import Form from "./components/Form";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
         <h1>Product Manager</h1>
         <br />
-        <Form />
+        <Main />
     </div>
   );
 }
